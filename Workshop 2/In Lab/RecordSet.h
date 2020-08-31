f8695dcd68070088968f1448af7704dd92b8fda7
dchan68 <54960101+dchan68@users.noreply.github.com>
2020-08-30 23:02:17 -0400

    Add files via upload

---
+++ Workshop 2/In Lab/RecordSet.h
@@ -0,0 +1,31 @@
+// Name: Daryan Chan
+// Seneca Student ID: 113973192
+// Seneca email: dchan68@myseneca.ca
+// Date of completion: 1/23/2020
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+
+
+#ifndef SDDS_RECORDSET_H
+#define SDDS_RECORDSET_H
+
+#include <string>
+
+namespace sdds {
+	class RecordSet {
+		unsigned long int counter;
+		std::string* s_array;
+	
+	public:
+		RecordSet();
+		RecordSet(const char*);
+		RecordSet(const RecordSet&);
+		RecordSet& operator=(const RecordSet&);
+		~RecordSet();
+		size_t size() const;
+		std::string getRecord(size_t);
+		void setEmpty();
+	};
+}
+#endif
